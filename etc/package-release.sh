a7b57e188e77e0089484ed54ce279e3e3926727b
Patrick Freed <patrick.freed@mongodb.com>
2021-04-15 11:35:38 -0400

    packaging stuff

---
+++ etc/package-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+
+if [ -z "${1+x}" ]; then
+    echo "version string must be specified as command line argument"
+    exit 1
+fi
+
+VERSION=${1}
+SOURCE_DIR="./mongodb-${VERSION}"
+
+if [ -d "$SOURCE_DIR" ]; then
+    rm -r ${SOURCE_DIR};
+fi
+cp -r src ${SOURCE_DIR}
+
+tar -cf "mongodb-${VERSION}.tar" ${SOURCE_DIR}
+
+rm -r ${SOURCE_DIR}
